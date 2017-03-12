7b37371a03acd2e0639a52c380ec48a8c5e5ad0a
wenyueshi <2236894781@qq.com>
2017-03-12 15:51:25 +0800

    Print

--- src/javatest1/Javatest1.java
+++ src/javatest1/Javatest1.java
@@ -15,6 +15,7 @@ public class Javatest1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("testgithub.Testgithub.main()");
         // TODO code application logic here
     }
     
